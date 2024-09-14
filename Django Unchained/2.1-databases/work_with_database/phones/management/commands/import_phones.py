2241b5467f90d0a7f0d2f43a721e794521bddf73
Eventoi <Githubt3st@mail.ru>
2024-09-14 19:53:02 +0300

    Django Unchained

--- Django Unchained/2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ Django Unchained/2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,16 +2,36 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.template.defaultfilters import slugify
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
-
+        parser.add_argument(
+            '-c',
+            '--create',
+            action='store_true',
+            default=False,
+            help='Импортирование данных'
+        )
+        
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        # Phone.objects.filter(all).delete()
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        if options['create'] or options['c']:
+            with open('phones.csv', 'r') as file:
+                phones = list(csv.DictReader(file, delimiter=';'))
+
+            for phone in phones:
+                # TODO: Добавьте сохранение модели
+                Phone.objects.create(
+                    name=phone.get('name'), 
+                    image=phone.get('image'), 
+                    price=phone.get('price'),
+                    release_date=phone.get('release_date'),
+                    lte_exists=phone.get('lte_exists'),
+                    slug=slugify(phone.get('name')),
+                )
+
+        else:
+            print('Для импортирования данных введите python manage.py import_phones -c')
--- Django Unchained/2.1-databases/work_with_database/phones/models.py
+++ Django Unchained/2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=255, verbose_name='Марка')
+    image = models.URLField(verbose_name='Внешний вид')
+    price = models.IntegerField(verbose_name='Цена')
+    release_date = models.DateField(auto_now_add=True, verbose_name='Дата изготовления')
+    lte_exists = models.BooleanField(verbose_name='Наличие товара')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return f'{self.name}, {self.price}, {self.release_date}'
