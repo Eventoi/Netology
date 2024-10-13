899be43cc7490e694258140f68374e9df5dd08d7
Eventoi <Githubt3st@mail.ru>
2024-10-14 01:00:18 +0300

    Умный дом

--- Django Unchained/3.1-drf-intro/smart_home/measurement/serializers.py
+++ Django Unchained/3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,30 @@
 from rest_framework import serializers
+from .models import Measurement, Sensor
 
 # TODO: опишите необходимые сериализаторы
+
+
+# class SensorSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+
+# ИЛИ ТАК
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description'] 
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['sensor', 'temperature', 'date']
+
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
