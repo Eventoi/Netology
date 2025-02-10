40422eda7cd451953fb8b8c75767a5f49eedf8fb
Eventoi <Githubt3st@mail.ru>
2025-02-10 23:27:20 +0300

    Домашнее задание

--- JS/js-features/mole-game/task.js
+++ JS/js-features/mole-game/task.js
@@ -1,15 +1,21 @@
 (() => {
   let wins = 0,
-      losses = 0;
+      losses = 0,
+      kills = 0,
+      misses = 0;
 
   const getHole = index => document.getElementById(`hole${index}`),
         resetGame = (message) => {
           alert(message);
           wins = 0;
           losses = 0;
+          kills = 0;
+          misses = 0;
           updateScore();
         },
         updateScore = () => {
+          document.getElementById('dead').textContent = kills;
+          document.getElementById('lost').textContent = misses;
           document.getElementById('wins').textContent = wins;
           document.getElementById('losses').textContent = losses;
         };
@@ -17,11 +23,13 @@
   for (let i = 1; i <= 9; i++) {
     getHole(i).onclick = () => {
       if (getHole(i).classList.contains('hole_has-mole')) {
+        kills++;
         wins++;
         if (wins === 10) {
           resetGame('Поздравляем! Вы победили!');
         }
       } else {
+        misses++;
         losses++;
         if (losses === 5) {
           resetGame('К сожалению, вы проиграли.');
