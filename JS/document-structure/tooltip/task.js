d7b1994897a5fca72ea0ef9080570d27e68617c6
Eventoi <Githubt3st@mail.ru>
2025-02-27 09:25:11 +0300

    Домашнее задание

--- JS/document-structure/tooltip/task.js
+++ JS/document-structure/tooltip/task.js
@@ -1,26 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   const tooltipElements = document.querySelectorAll('.has-tooltip');
-
+  
   tooltipElements.forEach(function(element) {
-
-    const tooltip = document.querySelector('.tooltip');
 
     element.addEventListener('click', function(event) {
       event.preventDefault();
+
+      const tooltip = document.createElement('div');
+      tooltip.classList.add('tooltip');
       const tooltipText = element.getAttribute('title');
-      tooltip.textContent = tooltipText;
+      tooltip.innerHTML = `${tooltipText} <a href="#" class="task__remove">&times;</a>`;
+
+      document.body.appendChild(tooltip);
+
       const rect = element.getBoundingClientRect();
       tooltip.style.top = rect.bottom + 5 + 'px';
       tooltip.style.left = rect.left + 'px';
 
-
       tooltip.classList.add('tooltip_active');
-      
+
+      const closeButton = tooltip.querySelector('.task__remove');
+      closeButton.addEventListener('click', function() {
+        tooltip.classList.remove('tooltip_active');
+        tooltip.remove();
+      });
 
       document.addEventListener('click', function closeTooltip(event) {
-        if (!element.contains(event.target)) {
+        if (!element.contains(event.target) && !tooltip.contains(event.target)) {
           tooltip.classList.remove('tooltip_active');
+          tooltip.remove();
           document.removeEventListener('click', closeTooltip);
         }
       });
